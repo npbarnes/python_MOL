865db1f5726a3d5d84f002ab2d774fe7f0fa0308
Nathan Barnes <npbarnes@alaska.edu>
2019-02-20 18:49:22 -0900

    Separate into a benchmarking routine and a plotting example

--- Heat_Eqn_MOL.py
+++ Heat_Eqn_MOL.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy.sparse as sp
-import matplotlib.pyplot as plt
 from time_steppers import quasilinear_trapezoid
 
 def build_matrix(dx, size):
@@ -12,9 +11,32 @@ def build_matrix(dx, size):
 
     return A.tocsr()
 
-if __name__ == '__main__':
-    domain_size = 100
+def benchmark():
+    domain_size = 10000
     dt = .001
+    dx = .1
+
+    q0 = np.zeros(domain_size+2, dtype='d')
+    q0[0] = 100.0
+    A = build_matrix(dx, domain_size)
+    Adense = A.toarray()
+
+    fd = quasilinear_trapezoid(0, dt, q0, A) # noqa: F841
+    fd_dense = quasilinear_trapezoid(0,dt,q0,Adense) # noqa: F841
+
+    import timeit
+    setup = 'from __main__ import fd'
+    time = timeit.timeit('fd.step(1)', setup=setup, number=3)
+    print(time)
+    setup_dense = 'from __main__ import fd_dense'
+    time_dense = timeit.timeit('fd_dense.step(1)', setup=setup_dense, number=3)
+    print(time_dense)
+    print(time_dense/time)
+
+def plot_heat():
+    import matplotlib.pyplot as plt
+    domain_size = 100
+    dt = .01
     dx = .1
 
     q0 = np.zeros(domain_size+2, dtype='d')
@@ -22,10 +44,13 @@ if __name__ == '__main__':
     A = build_matrix(dx, domain_size)
 
     fd = quasilinear_trapezoid(0, dt, q0, A)
-    fd.step(1000)
-    plt.plot(fd.q)
-    fd.step(1000)
-    plt.plot(fd.q)
-    fd.step(1000)
-    plt.plot(fd.q)
+
+    plt.plot(fd.q, label=f'time = {round(fd.t)}')
+    for i in range(3):
+        fd.stepBy(1)
+        plt.plot(fd.q, label=f'time = {round(fd.t)}')
+
+    plt.legend()
     plt.show()
+
+plot_heat()
